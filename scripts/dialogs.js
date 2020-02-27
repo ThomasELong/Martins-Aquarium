d27e036aa6ff5f8a77ddbd9f746658d9ecdcadcc
Thomas Long <thomas@Thomass-MacBook-Pro.local>
2020-02-27 10:59:03 -0600

    uploaded old format, began automating code input

---
+++ scripts/dialogs.js
@@ -0,0 +1,77 @@
+const initializeDetailButtonEvents = () => {
+    // CAN'T TOUCH THIS - START
+    const allCloseButtons = document.querySelectorAll(".button--close")
+
+    for (const btn of allCloseButtons) {
+        btn.addEventListener("click", theEvent => {
+                const dialogElement = theEvent.target.parentNode
+                dialogElement.close()
+            }
+        )
+    }
+    // CAN'T TOUCH THIS - END
+
+
+    // You will be writing code below this line
+
+    // Show Bart's details when the button is clicked
+    document.querySelector("#button--bart").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#details--bart")
+            theDialog.showModal()
+        }
+    )
+// Show Lucy's details when the button is clicked
+    document.querySelector("#button--Lucy").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#details--Lucy")
+            theDialog.showModal()
+        }
+    )
+    // Show Betty's details when the button is clicked
+    document.querySelector("#button--betty").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#details--betty")
+            theDialog.showModal()
+        }
+    )
+
+    // Show Nemo's details when the button is clicked
+    document.querySelector("#button--nemo").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#details--nemo")
+            theDialog.showModal()
+        }
+    )
+
+    // Show Dory's details when the button is clicked
+    document.querySelector("#button--dory").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#details--dory")
+            theDialog.showModal()
+        }
+    )
+     // Show Jeff's details when the button is clicked
+     document.querySelector("#button--jeff").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#details--jeff")
+            theDialog.showModal()
+        }
+     )
+     // Show Keke's details when the button is clicked
+     document.querySelector("#button--Keke").addEventListener(
+        "click",
+        theClickEvent => {
+            const theDialog = document.querySelector("#details--Keke")
+            theDialog.showModal()
+        }
+    )
+}
+
+export default initializeDetailButtonEvents
---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+import initializeDetailButtonEvents from './dialogs.js'
+import { useFish } from './FishDataProvider.js'
+
+initializeDetailButtonEvents()
+
+const arrayOfTheFishObjects = useFish()
+
+
+
+
+
