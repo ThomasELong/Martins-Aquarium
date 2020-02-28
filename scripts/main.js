d727716545818ef6eca7e46dff75958808400186
Thomas Long <thomas@Thomass-MacBook-Pro.local>
2020-02-28 11:21:40 -0600

    with the help of my friendly neighbors i fixed the buttons

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,11 @@ import initializeDetailButtonEvents from './dialogs.js'
 import { useFish } from './FishDataProvider.js'
 import { FishList } from './FishList.js'
 
-
-// initializeDetailButtonEvents()
-
 FishList()
+
+initializeDetailButtonEvents()
+
+
 
 
 
