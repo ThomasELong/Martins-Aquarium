fb95d190a3e0e80b603e07a259b4cd3fb70f8134
Thomas Long <thomas@Thomass-MacBook-Pro.local>
2020-02-28 10:54:20 -0600

    finished rough formatting so that it's easier to see

--- scripts/Fish.js
+++ scripts/Fish.js
@@ -5,22 +5,20 @@
 const Fish = (fish) => {
     return `
         <div class="fish">
-            <div class="fish__details">
-                <div>
-                    <img class="fish__image" src="${fish.image}" alt="">
-                </div>
-                <div class="fish__name">${fish.name}</div>
-                <button id="button--${fish.name}">Details</button>
-
-                <dialog class="dialog--fish" id="details--${fish.name}">
-                    <div>Species: ${fish.species}</div>
-                    <div>Location: ${fish.location}</div>
-                    <div>Length: ${fish.size}</div>
-                    <div>Food: ${fish.food.join(",")}</div>
-
-                    <button class="button--close">Close Dialog</button>
-                </dialog>
+            <div>
+                <img class="fish__image" src="${fish.image}" alt="">
             </div>
+            <div class="fish__name">${fish.name}</div>
+            <button id="button--${fish.name}">Details</button>
+
+            <dialog class="dialog--fish" id="details--${fish.name}">
+                <div>Species: ${fish.species}</div>
+                <div>Location: ${fish.location}</div>
+                <div>Length: ${fish.size}</div>
+                <div>Food: ${fish.food.join(",")}</div>
+
+                <button class="button--close">Close Dialog</button>
+            </dialog>
         </div>
     `
 }
